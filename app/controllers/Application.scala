ee96d6c17843022cca01716be2ecd125da93a0d8
AlexRimmerQA <alex.rimmer@qa.com>
2017-07-27 10:53:12 +0100

    Added a delete to the functionality, changed hypertext to buttons, locked the id field in edit to prevent problems

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -16,11 +16,21 @@ class Application @Inject()(val messagesApi: MessagesApi) extends Controller wit
     Ok(views.html.getitems(Item.items))
   }
 
-  def editItem(name: Option[String]): Action[AnyContent] = Action {
-    name match {
-      case Some(x) => Ok(views.html.edititem(Item.items.filter(item => item.name == name.get).head))
+  def editItem(id: Option[Int]): Action[AnyContent] = Action {
+    id match {
+      case Some(x) => Ok(views.html.edititem(Item.createItemForm.fill(Item.items.filter(item => item.id == id.get).head)))
       case None => NotFound(<h1>Unable to find this webpage</h1>)
     }
+  }
+
+  def editItemData() : Action[AnyContent] = Action { implicit request =>
+    val formValidationResult = Item.createItemForm.bindFromRequest
+    formValidationResult.fold({ formWithErrors =>
+      BadRequest(views.html.edititem(formWithErrors))
+    },{ item =>
+      Item.items.update(Item.items.indexWhere(listItem => listItem.id == item.id), item)
+      Redirect(routes.Application.getItems())
+    })
   }
 
   def addItem() : Action[AnyContent] = Action { implicit request =>
@@ -33,6 +43,23 @@ class Application @Inject()(val messagesApi: MessagesApi) extends Controller wit
       BadRequest(views.html.additem(formWithErrors))
     },{ item =>
       Item.items.append(item)
+      Redirect(routes.Application.getItems())
+    })
+  }
+
+  def deleteItem(id: Option[Int]) : Action[AnyContent] = Action { implicit request =>
+    id match {
+      case Some(x) => Ok(views.html.deleteitem(Item.createItemForm.fill(Item.items.filter(item => item.id == x).head)))
+      case None => NotFound(<h1>Unable to find this webpage</h1>)
+    }
+  }
+
+  def deleteItemData() : Action[AnyContent] = Action { implicit request =>
+    val formValidationResult = Item.createItemForm.bindFromRequest
+    formValidationResult.fold({ formWithErrors =>
+      BadRequest(views.html.edititem(formWithErrors))
+    },{ item =>
+      Item.items.remove(Item.items.indexWhere(listItem => listItem.id == item.id))
       Redirect(routes.Application.getItems())
     })
   }
