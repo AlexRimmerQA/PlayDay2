a79c0756a32a835af8f05d6f6ad76044d6b55161
AlexRimmerQA <alex.rimmer@qa.com>
2017-07-28 11:32:50 +0100

    Added update functionality to the mongo integration

---
+++ app/controllers/MyController.scala
@@ -0,0 +1,48 @@
+package controllers
+
+import javax.inject.Inject
+
+import scala.concurrent.Future
+import play.api.mvc.{Action, AnyContent, Controller}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json._
+import reactivemongo.api.Cursor
+import models._
+import models.JsonFormats._
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import reactivemongo.play.json._
+import collection._
+
+class MyController @Inject() (val reactiveMongoApi: ReactiveMongoApi) extends Controller with MongoController with ReactiveMongoComponents {
+  def collection: Future[JSONCollection] = database.map(_.collection[JSONCollection]("persons"))
+
+  def create: Action[AnyContent] = Action.async {
+    val user = User(29, "FirstName", "Lastname", List(Feed("BBC news", "http://www.bbc.co.uk")))
+    val futureResult = collection.flatMap(_.insert(user))
+    futureResult.map(_ => Ok("Added user " + user.firstName + " " + user.lastName))
+  }
+
+  def findByName: Action[AnyContent] = Action.async {
+    val cursor: Future[Cursor[User]] = collection.map {
+      _.find(Json.obj("lastName" -> "LastNaem"))
+        .sort(Json.obj("created" -> -1))
+        .cursor[User]
+    }
+    val futureUsersList: Future[List[User]] = cursor.flatMap(_.collect[List]())
+    futureUsersList.map { persons =>
+      Ok(persons.head.toString)
+    }
+  }
+
+  def update : Action[AnyContent] = Action.async { implicit request =>
+    val user = User(29, "FirstName", "LastNaem", List(Feed("BBC news", "http://www.bbc.co.uk")))
+    Console.println(user)
+    val modifier = Json.obj("$set" -> Json.obj("lastName" -> user.lastName))
+    Console.println(modifier)
+    val futureResult = collection.flatMap(_.update(Json.obj("lastName" -> "Lastname"), modifier))
+    futureResult.map { _ =>
+      Console.println("Inside update, it found the match")
+      Redirect(routes.MyController.findByName())
+    }
+  }
+}
--- build.sbt
+++ build.sbt
@@ -8,8 +8,19 @@ resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
       
 scalaVersion := "2.11.11"
 
-libraryDependencies ++= Seq( jdbc , cache , ws , specs2 % Test )
+libraryDependencies ++= Seq(
+  jdbc ,
+  cache ,
+  ws ,
+  specs2 % Test
+)
 
-unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
+// only for Play 2.5.x
+libraryDependencies ++= Seq(
+  "org.reactivemongo" %% "play2-reactivemongo" % "0.12.5-play25"
+)
 
-      
+
+unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )
+
+routesGenerator := InjectedRoutesGenerator
